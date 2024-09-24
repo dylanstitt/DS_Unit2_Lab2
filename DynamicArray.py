76d1848406f922e743abb5596fd55783ba04dc3b
Dylan Stitt <dstitt527@gmail.com>
2024-09-24 13:50:16 -0400

    final

--- DynamicArray.py
+++ DynamicArray.py
@@ -1,4 +1,56 @@
+import ctypes
+
 class DynamicArray:
 
     def __init__(self):
-        ...
+        """Creates the array with a default cap of 1"""
+        self.__n = 0
+        self.__capacity = 1
+        self.__A = []
+
+    def __str__(self):
+        """To-String"""
+        if self.__n == 0:
+            return "[]"
+
+        out = '['
+        for i, element in enumerate(self.__A):
+            try:
+                out += str(element) + ', '
+                temp = self.__A[i + 1]
+            except:
+                break
+        return out[:-2] + ']'
+
+    def __len__(self):
+        """Returns the length of the array"""
+        return len(self.__A)
+
+    def __getitem__(self, index):
+        """Return element at index"""
+        if index <= 0 or index >= self.__n:
+            raise IndexError("invalid index")
+
+        return self.__A[index]
+
+    def __make_array(self, c):
+        """Return new array with capacity c"""
+        return (c * ctypes.py_object)()
+
+    def __resize(self):
+        """Changes the capacity of the array when we reach max capacity"""
+        self.__capacity *= 2
+
+    def append(self, element):
+        """Add a new element to the array and increase __n + 1 and increase __capacity if needed"""
+        if self.__capacity == self.__n:
+            self.__resize()
+
+        tempArr = self.__A + [element]
+        self.__A = self.__make_array(self.__capacity)
+        self.__A = tempArr
+        self.__n += 1
+
+    def get_cap(self):
+        """Returns the capacity of the array"""
+        return self.__capacity
